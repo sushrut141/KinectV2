5c4f0da469cfffa58e29f4e9ef2dd121e5b1f987
sushrut141 <sushrut.shivaswamy@gmail.com>
2015-06-10 09:20:44 +0530

    Utility to view a .ply file as a Point Cloud using PCL visualizer

---
+++ utils/plyviewer/plyparse.cpp
@@ -0,0 +1,117 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+
+//PCL
+#include <pcl/common/common_headers.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/console/parse.h>
+#include <pcl/point_types.h>
+
+struct Point 
+{
+	Point(float x,float y,float z,unsigned char r,unsigned char g,unsigned char b):
+	x(x),y(y),z(z),r(r),g(g),b(b){}
+	float x,y,z;
+	float r,g,b;
+	void print()
+	{
+		std::cout<<"Vertex"<<" "<<x<<" "<<y<<" "<<z<<std::endl;
+		std::cout<<"color"<<" "<<r<<" "<<g<<" "<<b<<std::endl;
+	}
+	void nornalizeColor()
+	{
+		r = r/255;g = g/255;b = b/255;
+	}
+};
+
+int main(int argc,char**argv)
+{
+	if(argc!=2){
+		std::cout<<"Enter path to .ply file to run"<<std::endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	long long int num,vertex_num;
+	std::ifstream inFile(argv[1]);
+
+	std::vector<Point> points;
+
+	std::string line,words;
+
+	while(std::getline(inFile,line))
+	{
+		std::istringstream streamer(line);
+		streamer>>words;
+		if(words.compare("ply")==0)
+			continue;
+		if(words.compare("format")==0)
+			continue;
+		if(words.compare("comment")==0)
+			continue;
+		if(words.compare("element")==0)
+		{
+			streamer >> words >>num;
+			if(words.compare("vertex")==0){
+				vertex_num = num;
+				continue;
+			}
+		}
+		if(words.compare("end_header")==0)
+			break;
+	}
+
+	std::cout<<"Number of vertices are "<<vertex_num<<std::endl;
+	for(long long int i =0;i < vertex_num; i++)
+	{
+		float x,y,z;
+		unsigned int r,g,b;
+		std::getline(inFile,line);
+		std::istringstream values(line);
+		values >> x >> y >> z;
+		values >> r >> g >> b;
+		points.push_back(Point(x,y,z,r,g,b));
+
+	}
+	/*
+	for(long long int i=0;i<vertex_num;i++){
+		points.at(i).nornalizeColor();
+		points.at(i).print();
+	}
+	*/
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+
+	cloud->points.resize(vertex_num);
+
+	for(long long int i=0;i<vertex_num;i++)
+	{
+		//vertex
+		cloud->points[i].x = points[i].x;
+		cloud->points[i].y = points[i].y;
+		cloud->points[i].z = points[i].z;
+		//color
+		cloud->points[i].r = points[i].r;
+		cloud->points[i].g = points[i].g;
+		cloud->points[i].b = points[i].b;
+	}
+
+	pcl::visualization::PCLVisualizer* viewer (new pcl::visualization::PCLVisualizer ("Kinect Cloud"));
+	pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
+	viewer->addPointCloud<pcl::PointXYZRGB>(cloud,rgb,"Kinect Cloud");
+    viewer->setBackgroundColor (0, 0, 0);
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE,3, "Kinect Cloud");
+	viewer->addCoordinateSystem (1.0);
+	viewer->initCameraParameters ();
+
+    viewer->updatePointCloud(cloud, "Kinect Cloud");
+	viewer->spinOnce (100000);
+
+	delete viewer;
+
+
+}
